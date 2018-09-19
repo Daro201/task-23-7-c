a559eabf41b49bf38f7c4fcd95cace3ec3756ffc
Adrian Meller <melleradria@gmail.com>
2018-09-19 20:16:17 +0200

    Inital commit

---
+++ client/modules/Lane/LaneContainer.js
@@ -0,0 +1,42 @@
+import { connect } from 'react-redux';
+import Lane from './Lane';
+import { createNoteRequest } from '../Note/NoteActions';
+import { fetchLanes, updateLaneRequest, editLane, deleteLaneRequest, moveBetweenLanes } from './LaneActions';
+import { compose } from 'redux';
+import { DropTarget } from 'react-dnd';
+import ItemTypes from '../Kanban/itemTypes';
+
+
+const mapStateToProps = (state, ownProps) => ({
+  laneNotes: ownProps.lane.notes.map(noteId => state.notes[noteId]),
+});
+
+const mapDispatchToProps = {
+  editLane,
+  moveBetweenLanes: moveBetweenLanes,
+  updateLane: updateLaneRequest,
+  deleteLane: deleteLaneRequest,
+  addNote: createNoteRequest,
+};
+
+const noteTarget = {
+ hover(targetProps, monitor) {
+   const sourceProps = monitor.getItem();
+   const { id: noteId, laneId: sourceLaneId } = sourceProps;
+
+   if (!targetProps.lane.notes.length) {
+     targetProps.moveBetweenLanes(
+       targetProps.lane.id,
+       noteId,
+       sourceLaneId,
+     );
+   }
+ },
+};
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  DropTarget(ItemTypes.NOTE, noteTarget, (dragConnect) => ({
+    connectDropTarget: dragConnect.dropTarget()
+  }))
+)(Lane);
